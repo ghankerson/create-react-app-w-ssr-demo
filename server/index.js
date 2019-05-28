3f37bf62536e7afc4a0cedcb25f3fb6842c8d86e
Geoff Hankerson <ghankerson@mor.org>
2019-05-28 12:11:06 -0500

    get basic SSR working

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import serverRenderer from './middleware/renderer';
+
+const PORT = 4000
+const path = require('path')
+
+const app = express()
+const router = express.Router()
+
+router.use('^/$', serverRenderer);
+
+router.use(express.static(
+    path.resolve(__dirname, '..', 'build'),
+    { maxAge: '30d' },
+));
+
+
+app.use(router);
+
+
+app.listen(PORT, (error) => {
+  if(error) {
+    return console.log('An error happened', error);
+  }
+
+  console.log(`Listening on port ${PORT}`)
+})
